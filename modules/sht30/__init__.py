108909146a91e6cf3c30e1d04ae8e6e91df67aa7
brzez <pawel@brzez.com>
2019-04-26 23:06:27 +0200

    deep sleep + read bat voltage

--- modules/sht30/__init__.py
+++ modules/sht30/__init__.py
@@ -4,11 +4,6 @@ import uasyncio as asyncio
 
 sensor = SHT30()
 
-temperature, humidity = sensor.measure()
-
-print('Temperature:', temperature, 'ºC, RH:', humidity, '%')
-
-
 def get_default_config():
     return dict(check_interval=10)
 
@@ -27,8 +22,21 @@ def boot(loop):
 
 
 async def tick():
+    import machine
     while True:
+        adc = machine.ADC(0)
+        battery_voltage = (adc.read() / 1023.0) * 4.2
         temperature, humidity = sensor.measure()
+
+        publish(b'bat_voltage', b'{}'.format(battery_voltage))
         publish(b'humidity', b'{}'.format(humidity))
         publish(b'temperature', b'{}'.format(temperature))
-        await asyncio.sleep(check_interval)
+
+        check_interval = 10 * 60 * 1000
+
+        print('going to sleep')
+        await asyncio.sleep(2)
+        rtc = machine.RTC()
+        rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+        rtc.alarm(rtc.ALARM0, check_interval)
+        machine.deepsleep()
