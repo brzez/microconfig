50b9050a2ba1df4a3c75477fa3016627c05a728b
brzez <pawel@brzez.com>
2019-04-28 20:44:35 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 from microconfig import init
-from modules.mqtt import subscribe
-from modules.webserver import route
 
 
 def test():
     init()
-
-
-@subscribe('foo')
-def asd(message):
-    print('message from topic {}'.format(message))
-
-
-@subscribe('bar')
-def basd(message):
-    print('message from topic {}'.format(message))
-
-
-@route('/foo')
-def foo(request, response):
-    response.content = 'foo'
 
 
 test()
